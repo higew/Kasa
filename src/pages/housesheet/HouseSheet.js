c2cc7ecff0853ebd99f0e2db70794efb9906021a
higew <hi.meyerb@gmail.com>
2023-01-11 15:12:51 +0100

    Begining of the house description view

--- src/pages/housesheet/HouseSheet.js
+++ src/pages/housesheet/HouseSheet.js
@@ -1,10 +1,77 @@
 import React from 'react'
+import Collapse from '../../components/collapse/Collapse';
+import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./houseSheet.css";
 
-const HouseSheet = () => {
-    return (
-        <div>
-        </div>
-    );
-};
+const Housing = () => {
+    const { id } = useParams();
+    const [appartment, setAppartment] = useState(null);
+    const navigate = useNavigate();
 
-export default HouseSheet;
+    useEffect(() => {
+        const getData = () => {
+        fetch("../../data.json", {
+            headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            },
+        })
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (myJson) {
+            let myAppartment = myJson.find((app) => app.id === id);
+            if (myAppartment) {
+                setAppartment(myAppartment);
+            } else {
+                navigate("/*");
+            }
+            });
+        };
+        getData();
+    }, [appartment, id, navigate]);
+
+    return appartment ? (
+        <section id={"appartment-description"}>
+            <div id={"container-gallery"}>
+                
+            </div>
+            <div id={"container-primary-infos"}>
+                <div id={"container-logement-infos"}>
+                    <h2 id={"active-name-location"}>{appartment.title}</h2>
+                    <p id={"active-place-location"}>{appartment.location}</p>
+                    <div className={"tags"}>
+                        
+                    </div>
+                </div>
+                <div id={"container-owner-stars"}>
+                    <div id={"container-owner"}>
+                        <div className={"owner"}>
+                            <p id={"owner-name"}>{appartment.host.name}</p>
+                            <img id={"owner-picture"} src={appartment.host.picture} alt={appartment.host.name} />
+                        </div>
+                    </div>
+                    <div id={"evaluation"}>
+                        
+                    </div>
+                </div>
+            </div>
+            <div id={"collapses"}>
+                <Collapse title={"Description"}>
+                <p className={"description-logement-collapse"}>
+                    {appartment.description}
+                </p>
+                </Collapse>
+                <Collapse title={"Équipements"}>
+                <ul className={"list-equipments"}>
+                    {appartment.equipments.map((equipment, index) => (
+                    <li key={index}>{equipment}</li>
+                    ))}
+                </ul>
+                </Collapse>
+            </div>
+        </section>
+    ) : null;
+}
+export default Housing;
